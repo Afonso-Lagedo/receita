6d42b37fffdfbfa25e9e638c85ebff79a6810793
Afonso-lagedo <afonso.lagedo@gmail.com>
2023-03-27 08:16:20 -0300

    finalização detalhes

--- src/app/componentes/detalhes/detalhes.component.ts
+++ src/app/componentes/detalhes/detalhes.component.ts
@@ -11,13 +11,40 @@ import { ReceitasService } from 'src/app/services/receitas.service';
 })
 export class DetalhesComponent {
   receita?:Receita;
+  ingredientesSelecionados: boolean[] = [];
+  passosSelecionados: boolean[] = [];
 
   constructor(private receitasService:ReceitasService, private route:ActivatedRoute){
-    this.getReceita()
+    this.getReceita();
   }
 
+  onCheckboxClick() {
+    setTimeout(() => {
+      const allIngredientesSelected = this.ingredientesSelecionados.every(selected => selected !== false);
+      const allPassosSelected = this.passosSelecionados.every(selected => selected !== false);
+      const allSelected = allIngredientesSelected && allPassosSelected;
+
+      const button = document.querySelector('button');
+      if (button) {
+        button.disabled = !allSelected;
+        if (allSelected) {
+          button.classList.remove('btnLaranja');
+          button.classList.add('btnVerde');
+        } else {
+          button.classList.remove('btnVerde');
+          button.classList.add('btnLaranja');
+        }
+      }
+    }, 50);
+  }
+  
+
   getReceita(){
-    const id = Number(this.route.snapshot.paramMap.get("id"));//extrair id
-    this.receitasService.getItem(id).subscribe((y)=> (this.receita=y));
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.receitasService.getItem(id).subscribe((y)=> {
+      this.receita=y;
+      this.ingredientesSelecionados = new Array(y.ingredientes.length).fill(false);
+      this.passosSelecionados = new Array(y.passos.length).fill(false);
+    });
   }
 }
